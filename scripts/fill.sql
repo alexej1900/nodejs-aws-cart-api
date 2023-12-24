6580df13e70dc8daffca7b7afb8bd3976843b07d
alexej1900 <alexej1900@gmail.com>
2023-12-24 13:51:34 +0100

    add: script

---
+++ scripts/fill.sql
@@ -0,0 +1,3 @@
+INSERT INTO carts(id, user_id, status) VALUES ('f45a8638-7c94-45b4-87d5-40fee1bd2f11', '63487908-cd1b-410e-8ca1-bdce2323232', 'OPEN');
+
+INSERT INTO cart_items(cart_id, product_id, count) VALUES ('f45a8638-7c94-45b4-87d5-40fee1bd2f11', '6e587908-cd1b-410e-8ca1-bdce6b68f181', 6);
